867cafe97af4349b4f0502d07b2d24f03075e4b2
aitesam961 <aitesam961@gmail.com>
2022-12-18 23:46:30 +0500

    Updated Catalog

---
+++ Low_Level_Implementation/ALU_V1.0/ALU_V1.0/ALU_Main.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:26:13 12/15/2022 
+// Design Name: 
+// Module Name:    ALU_Main 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ALU_Main(
+    input [15:0] data_in_1,
+    input [15:0] data_in_2,
+    input [1:0] alu_op,
+    output [15:0] data_out,
+    output z_flag,
+    output a_grt_b,
+    output b_grt_a
+    );
+	 
+	 
+	 
+
+
+endmodule
---
+++ Low_Level_Implementation/readme.md.txt
@@ -0,0 +1,1 @@
+https://inst.eecs.berkeley.edu/~cs61c/su09/assignments/proj/03/
